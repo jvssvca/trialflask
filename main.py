5527b380b1015276c532310d18ff2c5221ddaf7a
jvssvca <80436571+jvssvca@users.noreply.github.com>
2022-05-01 19:24:33 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, flash
+from flask_login import login_required, current_user
+from __init__ import create_app, db
+
+
+main = Blueprint('main', __name__)
+
+@main.route('/') # home page that return 'index'
+def index():
+    return render_template('index.html')
+
+@main.route('/memberships')
+def memberships():
+    return render_template('memberships.html')
+
+@main.route('/profile') # profile page that return 'profile'
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
+
+@main.route('/trainingschedule') 
+@login_required
+def training():
+    return render_template('training.html')
+
+@main.route('/admin') 
+@login_required
+def admin():
+    return render_template('admin.html')
+
+app = create_app() # we initialize our flask app using the __init__.py function
+if __name__ == '__main__':
+    db.create_all(app=create_app()) # create the SQLite database
+    app.run(debug=True) # run the flask app on debug mode
